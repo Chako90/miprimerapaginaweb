82b905f25875f4aea8129548d6fad95b0c10577e
Chako90 <148507207+Chako90@users.noreply.github.com>
2023-10-19 20:19:09 -0600

    fixes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 let menu_responsive = document.querySelector(".checkbtn");
 
 menu_responsive.onclick = function () {
-  navBar = document.querySelector("nav");
+  navBar = document.querySelector(".navbar");
   navBar.classList.toggle("active");
 };
