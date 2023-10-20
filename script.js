373283be77232e2f1149e96f85490735952d0e98
Chako90 <148507207+Chako90@users.noreply.github.com>
2023-10-19 18:25:23 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let menu_responsive = document.querySelector(".checkbtn");
+
+menu_responsive.onclick = function () {
+  navBar = document.querySelector("nav");
+  navBar.classList.toggle("active");
+};
